621e25b08a9cd79d9b59fafc49f48af77211b1fe
sshrimali <shrimali.sourabh@gmail.com>
2016-06-17 18:47:30 -0700

    updated test

--- testrail/testRailResultUpload.py
+++ testrail/testRailResultUpload.py
@@ -1,5 +1,22 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('results.xml')
-root = tree.findall("testcase")
-for testcase in root:
-    print testcase.get('name')
+
+import sys
+
+
+def getTestIds(filename):
+    tree = ET.parse(fileName)
+    root = tree.findall("testcase")
+    for testcase in root:
+        print testcase.get('name')
+
+
+def getArgs():
+    fileName=sys.argv[1];
+    return fileName
+
+def updateResults():
+    print ("test");
+
+fileName=getArgs()
+getTestIds(fileName)
+updateResults()
